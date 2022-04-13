c64525f6ef70d18ef4a6f1753d9761bb063497a1
Brad <walker.brad26@gmail.com>
2022-04-12 23:17:46 -0500

    Topbar icons, info, styling

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
-//import styles fromm css
-import "./topbar.css"
+import "./topbar.css" //css styleSheet 
+import {Person, Mail, Code} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
@@ -7,17 +7,33 @@ export default function Topbar() {
        <div className="wrapper">
 
          <div className="left">
-           <a href="#Intro">intro</a>
+
+           <a href="#Intro" className="logo">
+             BmW
+           </a>
+
+           <div className="itemContainer">
+             <Person className="icon"/>
+             <span>+1 618-402-4102</span>
+            </div>
+
+            <div className="itemContainer">
+              <Mail className="icon"/>
+              <span>walker.brad26@gmail.com</span>
+            </div>
+                   {/*
            <a href="#Skills">skills</a>
            <a href="#Projects">projects</a>
            <a href="#Contact">contact</a>
-
+                   */}
          </div>
          
-         <h1>Hello</h1>
+
+         <h1><Code/></h1>
+
 
          <div className="right">
-           this is right
+           right side
          </div>
 
        </div>
