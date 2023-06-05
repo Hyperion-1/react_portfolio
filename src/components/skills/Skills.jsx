f37f305b2413b5c22cc3b2f27b8b504b89b25626
Brad <walker.brad26@gmail.com>
2023-06-05 10:44:37 -0500

    skill list

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,8 +3,21 @@ import "./skills.css"
 export default function Skills() {
   return (
     <div className='Skills' id="Skills">
-       skills
-        
+       skills:
+
+       HTML 
+       CSS
+       JavaScript
+       React 
+       MySQL 
+       PHP(ish)
+       C++ 
+       Java 
+       Git 
+       GitHub
+       Node.js
+
+
     </div>
   )
 }
