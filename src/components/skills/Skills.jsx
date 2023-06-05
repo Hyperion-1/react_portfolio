547082ed457a52875b7a5e161d34606203a509f9
Brad <walker.brad26@gmail.com>
2023-06-05 11:14:32 -0500

    got *someting* on skill imgs

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,13 +1,23 @@
 import "./skills.css"
 
+import HTML5Icon from "/home/brad/Nu_repos/react_portfolio/src/assets/html5-icon.png"
+import CSSIcon from "/home/brad/Nu_repos/react_portfolio/src/assets/CSS3.png"
+import JSIcon from "/home/brad/Nu_repos/react_portfolio/src/assets/javascript.png"
+
 export default function Skills() {
   return (
     <div className='Skills' id="Skills">
        skills:
 
-       HTML 
+       HTML
+       <img src={HTML5Icon} width="100" height="200"></img>
+      
        CSS
+       <img src={CSSIcon} width="100" height="200"></img>
+
        JavaScript
+       <img src={JSIcon} width="100" height="200"></img>
+       
        React 
        MySQL 
        PHP(ish)
