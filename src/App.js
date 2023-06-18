d216b2ece9a49582252cae96debbb17a85e676e4
Brad <walker.brad26@gmail.com>
2023-06-18 18:04:25 -0500

    wrapped props in divs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss"
 
 //React components
-import Topbar from "./components/topbar/Topbar";
+//import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Skills from "./components/skills/Skills";
 import Projects from "./components/projects/Projects";
@@ -12,19 +12,17 @@ import Contact from "./components/contact/Contact";
 function App() {
   return (
     
-    <div className="app">
+    <div className="app">     
+     <div className="app-grid-container">
+      
+       <div className="introDiv-flex grid-item">     <Intro/>    </div>
+       <div className="skillsDiv">    <Skills/>   </div>
+       <div className="projectsDiv">  <Projects/> </div>
+       <div className="contactDiv">   <Contact/>  </div>
 
-     <Topbar/> 
-     
-     <div className="sections">
-       <Intro/>
-       <Skills/>
-       <Projects/>
-       <Contact/>       
       </div> 
       
     </div>
-
   );
 }
 
