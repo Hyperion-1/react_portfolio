37eb97501ef1742edaedfd6e61f22e80ced7dae0
Brad <walker.brad26@gmail.com>
2023-06-18 17:11:30 -0500

    redo topbar, removing for now

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,60 +1,13 @@
-import react, {useState} from 'react';
-
 import "./Topbar.scss"
-import {Person, Mail, Code} from "@material-ui/icons"
+
+//import react, {useState} from 'react';
+//import {Person, Mail, Code} from "@material-ui/icons"
 
 
 export default function Topbar() {
   return (
     <div className='Topbar'>
-       <div className="wrapper">
-
-
-
-         <div className="left">
-
-           <a href="#Intro" className="logo">
-             BmW
-           </a>
-
-
-
-          {/*>topbar contact info is neg. effecting css, may delete*/
-          /*
-           <div className="itemContainer">
-             <Person className="icon"/>
-             <span>+1 618-402-4102</span>
-            </div>
-
-            <div className="itemContainer">
-              <Mail className="icon"/>
-              <span>walker.brad26@gmail.com</span>              
-            </div>
-
-  */  }
-
-                   {/*
-           <a href="#Skills">skills</a>
-           <a href="#Projects">projects</a>
-           <a href="#Contact">contact</a>
-                   */}
-         </div>
-         
-
-        <h1><Code/></h1>
-
-
-          {/*hamburger menu*/}         
-        <div className="right">
-           <div className="hamburger">
-               <span classname='burgerBar'></span>
-               <span classname='burgerBar'></span>
-               <span classname='burgerBar'></span>    
-           </div>
-           
-         </div>
-
-       </div>
-    </div>/*MAIN DIV WRAPPER*/
+            {/* throwing this out for now, redo */}
+    </div>
   )
 }
