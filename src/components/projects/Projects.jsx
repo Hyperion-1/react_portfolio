daec4d11dff6793d758a772f7e707df11e5095dd
Brad <walker.brad26@gmail.com>
2023-06-19 11:12:43 -0500

    *attempted* basic 3 item mockup

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,23 @@
-//import styles fromm css
 import "./Projects.scss"
 
 export default function Projects() {
   return (
     <div className='Projects' id="Projects">
-        projects
+
+      <h1 className="projectsHeader">
+        <b><strong> My Projects </strong></b> 
+      </h1>
+
+      <div className="projects-grid-container">
+        <div className="flex-container">
+
+         <div className="flex-item-1">1</div>
+         <div className="flex-item-2">2</div>
+         <div className="flex-item-3">3</div>
+
+         </div>
+      </div>
+
     
     </div>
   )
