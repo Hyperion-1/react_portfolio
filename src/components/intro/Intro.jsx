8ab8cec386950bf9d7a7a8695a2fe3ef557973ee
Brad <walker.brad26@gmail.com>
2023-06-22 18:50:54 -0500

    basic background working

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,24 +2,26 @@ import "./Intro.scss"
 
 export default function Intro() {
   return (    
-    <div className='Intro'>
-      <body className="grid-wrapper">
-        
+    <div className='Intro'>   
+      <div className="grid-wrapper">     
      
          
 
 
 
-        <div className="intro-backboard">
-          
-         intro/landing
+         <div className="intro-backboard">
+
+          <div className="testTest">
+            <h1>intro/landing</h1>
+            <h2>Full-Stack Developer</h2>
+          </div>
        
         </div>
 
 
 
-    
-     </body>
+      </div>
+     
     </div>
   )
 }
